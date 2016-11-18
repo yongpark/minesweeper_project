4a48995c7130ef2608a9f6a7d288bbe6a2802a57
App Academy Student <student@appacademy.io>
2016-11-17 18:09:45 -0800

    made some bomb changes

--- minesweeper/board.rb
+++ minesweeper/board.rb
@@ -1,3 +1,5 @@
+require_relative 'tile'
+
 class Board
   attr_accessor :grid
 
@@ -5,20 +7,69 @@ class Board
     @grid = grid
   end
 
-  def populate
+
+  def [](pos)
+   x, y = pos
+   @grid[x][y]
+  end
+
+  def []=(pos, val)
+    x, y = pos
+    @grid[x][y] = val
+  end
+
+  def bomb_pos
+    result = []
+    until result.length == 10
+      x = rand(9)
+      y = rand(9)
+      result << [x, y] unless result.include?([x, y])
+    end
+    result
+  end
+
+  def populate_tiles
+    bomb_pos.each do |pos|
+      self[pos] = Tile.new("bomb")
+    end
+     @grid.each do |row|
+       row.each do |tile|
+        tile = Tile.new
+       end
+     end
   end
 
   def reveal(pos)
+    if self[pos] == true
+      self[pos].flip_tile
+    else
+      raise "Already flipped over. :<"
+    end
   end
 
   def flag(pos)
   end
 
   def render
-
+    top_line = "  " + (0..8).to_a.map { |el| el.to_s + "  " }.join
+    body = top_line + "\n"
+    @grid.each_with_index do |row, i|
+      current_str = "#{i}|"
+      row.each do |el|
+        current_str << " 🀄 " if el.value.nil?
+        if el.value == "bomb" && !el.hidden
+          current_str << " 💣 "
+        else
+          current_str << " 🀄 "
+        end
+      body << current_str + "\n"
+    end
+    puts body
   end
-
 end
 
 board = Board.new
-p board.grid
+board.grid
+board.bomb_pos
+board.populate_tiles
+board.render
--- minesweeper/tile.rb
+++ minesweeper/tile.rb
@@ -1,8 +1,13 @@
 class Tile
+  attr_reader :value, :hidden
 
   def initialize(value = nil)
     @value = value
+    @hidden = true
   end
 
-  
+  def flip_tile
+    @hidden = false
+  end
+
 end
